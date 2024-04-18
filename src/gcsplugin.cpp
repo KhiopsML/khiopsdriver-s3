cad1c6e1a0b42b6275bd20ee4e96c3b6de71721e
Stephane Gouache <stephane.gouache@orange.com>
2024-04-18 14:56:36 +0200

    Try fix compilation

--- src/gcsplugin.cpp
+++ src/gcsplugin.cpp
@@ -242,7 +242,6 @@ void *driver_fopen(const char *filename, char mode)
     ParseGcsUri(filename, bucket_name, object_name);
     FallbackToDefaultBucket(bucket_name);
 	assert(driver_isConnected());
-	assert((mode == 'r' or mode == 'w' or mode == 'a'));
 
     auto h = new MultiPartFile;
     h->bucketname = bucket_name;
