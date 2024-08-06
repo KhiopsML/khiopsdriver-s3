b564f94fe06aa6d67f18227f880e3c3828b8e899
Cedric LE CAM <cedric.lecam@sii.fr>
2024-08-06 18:33:48 +0200

    Add a utility to test pattern matching with globing according to the gitignore style.
    Formatting done by clang-format.

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -18,12 +18,117 @@
 #include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
 
 #include "../src/s3plugin.h"
+#include "contrib/matching.h"
 
 constexpr int kSuccess{1};
 constexpr int kFailure{0};
 
 constexpr int kFalse{0};
 constexpr int kTrue{1};
+
+void TestPatternMatching(const std::vector<std::string> &must_match,
+                         const std::vector<std::string> &no_match,
+                         const std::string &pattern) {
+  for (auto &s : must_match) {
+    ASSERT_TRUE(utils::gitignore_glob_match(s, pattern));
+  }
+
+  for (auto &s : no_match) {
+    ASSERT_FALSE(utils::gitignore_glob_match(s, pattern));
+  }
+}
+
+#define DO_PATTERN_MATCHING_TEST                                               \
+  TestPatternMatching(must_match, no_match, pattern)
+
+// tests for glob matching utility
+TEST(S3DriverMatchingUtilityTest, NoWildCard) {
+  const std::string pattern = "s3://this/pattern/to/match/exactly/A000.txt";
+
+  const std::vector<std::string> must_match = {pattern};
+
+  const std::vector<std::string> no_match = {
+      "", " ", "off_topic",
+      "s3://this/pattern/to/match/exactly/a000.txt", // case sensitivity
+      "s3://this/patern/to/match/exactly/A000.txt",  // error on the path
+      "s3://this/pattern/to/match/exactly/A000.tx",  // missing character at the
+                                                     // end
+      "3://this/pattern/to/match/exactly/A000.txt",  // missing character at the
+                                                     // beginning
+      "d3://this/pattern/to/match/exactly/A000.txt", // wrong character at the
+                                                     // begininng
+      "s3://this/pattern/to/match/exactly/A000.txx"  // wrong character at the
+                                                     // end
+  };
+
+  DO_PATTERN_MATCHING_TEST;
+}
+
+TEST(S3DriverMatchingUtilityTest, SimpleWildCard) {
+  const std::string pattern = "s3://path/to/dir/A00?.txt";
+
+  const std::vector<std::string> must_match = {
+      "s3://path/to/dir/A000.txt", "s3://path/to/dir/A00a.txt",
+      "s3://path/to/dir/A00-.txt", "s3://path/to/dir/A00?.txt"};
+
+  const std::vector<std::string> no_match = {"",
+                                             " ",
+                                             "off_topic",
+                                             "s3://path/to/dir/A00.txt",
+                                             "s3://path/to/dir/A00/a.txt",
+                                             "s3://path/to/dir/A0000.txt"};
+
+  DO_PATTERN_MATCHING_TEST;
+}
+
+TEST(S3DriverMatchingUtilityTest, MultiCharWildCard) {
+  const std::string pattern = "s3://path/to/dir/*.txt";
+
+  const std::vector<std::string> must_match = {"s3://path/to/dir/a.txt",
+                                               "s3://path/to/dir/aa.txt",
+                                               "s3://path/to/dir/1.txt"
+                                               "s3://path/to/dir/00.txt"};
+
+  const std::vector<std::string> no_match = {
+      "", " ", "off_topic",
+      "s3://path/to/a.txt",      // path does not match
+      "s3://path/to/dir/a/a.txt" // path does not match
+  };
+
+  DO_PATTERN_MATCHING_TEST;
+}
+
+TEST(S3DriverMatchingUtilityTest, NumericRangeWildCard) {
+  const std::string pattern = "s3://path/to/dir/[0-9].txt";
+
+  const std::vector<std::string> must_match = {
+      "s3://path/to/dir/0.txt",
+      "s3://path/to/dir/9.txt",
+  };
+
+  const std::vector<std::string> no_match = {
+      "", " ", "off_topic",
+      "s3://path/to/dir/a.txt", // not in the range
+      "s3://path/to/dir/00.txt" // too many characters
+  };
+
+  DO_PATTERN_MATCHING_TEST;
+}
+
+TEST(S3DriverMatchingUtilityTest, DoubleStarWildCard) {
+  const std::string pattern = "s3://path/**/a.txt";
+
+  const std::vector<std::string> must_match = {
+      "s3://path/to/dir/a.txt", "s3://path/to/a.txt", "s3://path/to/../a.txt"};
+
+  const std::vector<std::string> no_match = {
+      "", " ", "off_topic",
+      "s3://to/dir/a.txt",     // prefix before ** does not match
+      "s3://path/to/dir/b.txt" // file name after ** does not match
+  };
+
+  DO_PATTERN_MATCHING_TEST;
+}
 
 TEST(S3DriverTest, GetDriverName) {
   ASSERT_STREQ(driver_getDriverName(), "S3 driver");
