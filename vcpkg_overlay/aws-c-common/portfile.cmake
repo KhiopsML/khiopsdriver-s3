cd46523208e83d33a3373867216a846defa7c581
Stephane Gouache <stephane.gouache@orange.com>
2024-09-25 14:27:13 +0200

    Typo

--- vcpkg_overlay/aws-c-common/portfile.cmake
+++ vcpkg_overlay/aws-c-common/portfile.cmake
@@ -4,8 +4,6 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 25da9356e36c87210bcdd95b007824288f36fd3ae4bdd757a1d3e88ef3cc8b65a0c1a31cbe338147949257c8e908c1721fc6297aeb0cbfe7cb89b4d7727dc2ad
     HEAD_REF master
-    PATCHES
-        disable-internal-crt-option.patch # Disable internal crt option because vcpkg contains crt processing flow
 )
 
 vcpkg_cmake_configure(
