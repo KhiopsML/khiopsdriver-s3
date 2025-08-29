5740de81f73722655b6949fccdf33312695417be
Stéphane Gouache <stephane.gouache@orange.com>
2025-08-29 19:10:01 +0200

    Fix conda build

--- conda/bld.bat
+++ conda/bld.bat
@@ -8,7 +8,7 @@ subst W: %CD%
 W:
 
 REM Configure project
-cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds\conda -S .
+cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -D BUILD_ENV=conda -B builds\conda -S .
 
 REM Build
 cmake --build builds\conda --parallel --target khiopsdriver_file_s3
