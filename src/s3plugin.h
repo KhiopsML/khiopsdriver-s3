090f3a55eb2d75ec985af93605fb1612aa747a5f
Stéphane Gouache <stephane.gouache@orange.com>
2025-09-02 10:30:04 +0200

    Update versions

--- src/s3plugin.h
+++ src/s3plugin.h
@@ -2,7 +2,7 @@
 
 #define KHIOPS_STR(s) #s
 // Driver version
-#define DRIVER_VERSION KHIOPS_STR(0.0.13)
+#define DRIVER_VERSION KHIOPS_STR(0.0.14)
 // Release versions must have 3 digits, for example KHIOPS_STR(1.2.0)
 // Alpha, beta ou release candidate have an extra suffix, for example :
 // - KHIOPS_STR(1.2.0-a.1)
