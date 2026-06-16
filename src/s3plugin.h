84f719075c491aa80c149c9770c29d8137d7bc2c
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-06-16 14:42:07 +0200

    Bump version

--- src/s3plugin.h
+++ src/s3plugin.h
@@ -2,7 +2,7 @@
 
 #define KHIOPS_STR(s) #s
 // Driver version
-#define DRIVER_VERSION KHIOPS_STR(0.0.23)
+#define DRIVER_VERSION KHIOPS_STR(0.0.24)
 
 // Release versions must have 3 digits, for example KHIOPS_STR(1.2.0)
 // Alpha, beta ou release candidate have an extra suffix, for example :
